7d0563a3b860e090dad5ea0f1600017baa6b1a6d
kdy565 <kdysisyphe@gmail.com>
2025-11-07 16:19:07 +0900

    error edit

--- week9/hw.R
+++ week9/hw.R
@@ -11,10 +11,11 @@ df_logit$Fat_meal           <- factor(df_logit$Fat_meal)
 df_logit$Drink              <- factor(df_logit$Drink)
 df_logit$Sex                <- factor(df_logit$Sex) 
 df_logit$Education          <- factor(df_logit$Education) 
+df_logit$age <- as.numeric(df_logit$age)
 
 str(df_logit[, c("Obesity","Physical_activity","Fat_meal","Drink","Sex","Education","age")])
 
-LOR5<-glm(Obesity~Physical_activity+Fat_meal+Drink+Sex+Education,data=df_logit,family=binomial)
+LOR5<-glm(Obesity~Physical_activity+age+Fat_meal+Drink+Sex+Education,data=df_logit,family=binomial)
 summary(LOR5)
 exp(coef(LOR5))
 exp(confint.default(LOR5))
@@ -27,7 +28,7 @@ OR_result <- cbind(
 OR_result
 
 #2)통계적으로 유의하지 않은 변수 제외하고 모형 확인 (Education)
-LOR6<-glm(Obesity~Physical_activity+Fat_meal+Drink+Sex,data=df_logit,family=binomial)
+LOR6<-glm(Obesity~Physical_activity+age+Fat_meal+Drink+Sex,data=df_logit,family=binomial)
 summary(LOR6)
 exp(coef(LOR6))
 exp(confint.default(LOR6))
